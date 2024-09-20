b0398a12f6326a502784c94bbe3510608f5cf58f
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:34 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/man-dbport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/man-dbport.git']]])
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/man-dbport.git'), string(name: 'PORT_DESCRIPTION', value: 'man utility' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/man-dbport.git'), string(name: 'PORT_DESCRIPTION', value: 'man utility' )]
         }
 }
